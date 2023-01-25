9e504dc3969440a4ea3c810e3d8640fd8d8e8518
= <f7lipe@me.com>
2023-01-25 13:10:17 -0300

    feat: Slides

---
+++ src/components/Slides/index.tsx
@@ -0,0 +1,51 @@
+import { Slide, SlideShow, Slider } from "./styles"
+import { useState, useEffect, useRef } from "react"
+
+interface SlidesProps {
+    images: {
+        id: number
+        url: string
+    }[]
+}
+
+function Slides({ images }: SlidesProps) {
+    const [currentSlide, setCurrentSlide] = useState(0)
+    const delay = 2500
+
+    const timeoutRef = useRef(null)
+
+    function resetTimeout() {
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current);
+      }
+    }
+
+    useEffect(() => {
+        resetTimeout();
+        // @ts-ignore
+        timeoutRef.current = setTimeout(
+          () =>
+          setCurrentSlide((prevIndex) =>
+              prevIndex === images.length - 1 ? 0 : prevIndex + 1
+            ),
+          delay
+        );
+    
+        return () => {
+          resetTimeout();
+        };
+      }, [currentSlide])
+
+    return (
+        <SlideShow>
+            <Slider 
+                style={{ transform: `translate3d(${-currentSlide * 100}%, 0, 0)` }}>
+                {images.map((image) => (
+                    <Slide key={image.id} src={image.url} />
+                ))}
+            </Slider>
+        </SlideShow>
+    )
+}
+
+export default Slides
