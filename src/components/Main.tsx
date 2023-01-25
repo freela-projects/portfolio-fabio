a4eb12ed1f74e3ccb2feace18b1dc32c824eb870
= <f7lipe@me.com>
2023-01-25 13:08:04 -0300

    Update Main.tsx
    Includes disablePadding property

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,5 +1,9 @@
 import styled from "styled-components"
 
+interface MainProps {
+    disablePadding?: boolean
+}
+
 const Main = styled.main`
     display: flex;
     flex-direction: column;
@@ -7,7 +11,7 @@ const Main = styled.main`
     justify-content: center;
     width: 100%;
     height: 100vh;
-    padding: 20px;
+    padding:  ${(props: MainProps) => props.disablePadding ? "0" : "20px"};;
 `
 
 export default Main
