12edec7e7d1109788e200fe69a7b1e1645a25851
= <f7lipe@me.com>
2023-01-25 13:08:27 -0300

    Create fake data for Home page

---
+++ src/data/fake-data/home.ts
@@ -0,0 +1,19 @@
+export const images = [
+    {
+        id: 1,
+        url: "https://static.wixstatic.com/media/62ab43_59f973e12f034b5da6ba682db57a95b3.jpg/v1/fill/w_1333,h_677,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/62ab43_59f973e12f034b5da6ba682db57a95b3.jpg",
+    },
+        
+    {
+        id: 2,
+        url: "https://static.wixstatic.com/media/62ab43_16054ac606bd49098338a6885c46a9b9.jpg/v1/fill/w_1333,h_677,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/62ab43_16054ac606bd49098338a6885c46a9b9.jpg",
+    },
+    {
+        id: 3,
+        url: "https://static.wixstatic.com/media/62ab43_ac22ec43dcea401a94b7b2159e87312a.jpg/v1/fill/w_1333,h_677,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/62ab43_ac22ec43dcea401a94b7b2159e87312a.jpg",
+    },
+    {
+        id: 4,
+        url: "https://static.wixstatic.com/media/62ab43_a775ad679b1e44ec8a6e73498614a1d5.jpg/v1/fill/w_1333,h_677,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/62ab43_a775ad679b1e44ec8a6e73498614a1d5.jpg"
+    }
+]
