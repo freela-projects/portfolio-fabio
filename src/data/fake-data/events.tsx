37dd273550f81b42617047ca36e390098b66f5e3
= <f7lipe@me.com>
2023-01-26 10:45:01 -0300

    fake data fot events page

---
+++ src/data/fake-data/events.tsx
@@ -0,0 +1,15 @@
+export const images = [
+    {
+        id: 1,
+        url: "https://static.wixstatic.com/media/62ab43_3a4da6801c0441b4bb343deb99c36f94.jpg/v1/fill/w_215,h_215,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/62ab43_3a4da6801c0441b4bb343deb99c36f94.jpg",
+    },
+        
+    {
+        id: 2,
+        url: "https://static.wixstatic.com/media/62ab43_1469d21436b347ebbf97a45ac8382f1f.jpg/v1/fill/w_215,h_163,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/62ab43_1469d21436b347ebbf97a45ac8382f1f.jpg",
+    },
+    {
+        id: 3,
+        url: "https://static.wixstatic.com/media/62ab43_9151d65372e142c3925aa1fb176457dc.jpg/v1/fill/w_215,h_173,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/62ab43_9151d65372e142c3925aa1fb176457dc.jpg",
+    },
+]
