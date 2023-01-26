a4e93138f48d92309490f8f3224f7fc288f0c4e0
= <f7lipe@me.com>
2023-01-26 10:46:19 -0300

    feat: events page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from './pages/Home'
 import Arts from "./pages/Arts"
+import Events from "./pages/Events"
 import './shared/App.css'
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/art" element={<Arts/>} />
+        <Route path="/eve" element={<Events/>} />
       </Routes>
     </BrowserRouter>
   )
