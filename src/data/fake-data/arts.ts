9cba8743c15d79ae28d69fd8bf234ae716ddda51
= <f7lipe@me.com>
2023-01-26 09:45:00 -0300

    fake data for Arts page

---
+++ src/data/fake-data/arts.ts
@@ -0,0 +1,35 @@
+export const images = [
+    {
+        id: 1,
+        url: "https://static.wixstatic.com/media/62ab43_e783930f906b404399fa1dce27a29e56.jpg/v1/fill/w_732,h_488,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/62ab43_e783930f906b404399fa1dce27a29e56.jpg",
+    },
+        
+    {
+        id: 2,
+        url: "https://static.wixstatic.com/media/62ab43_cfb112be762e45ca916df58d28a9ebc1.jpg/v1/fill/w_325,h_488,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/62ab43_cfb112be762e45ca916df58d28a9ebc1.jpg",
+    },
+    {
+        id: 3,
+        url: "https://static.wixstatic.com/media/62ab43_e938547e062b4885a76ce5b5ec01ec15.jpg/v1/fill/w_325,h_488,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/62ab43_e938547e062b4885a76ce5b5ec01ec15.jpg",
+    },
+    {
+        id: 4,
+        url: "https://static.wixstatic.com/media/62ab43_8c46e9fa8a3b43fab85f73137ed9138f.jpg/v1/fill/w_325,h_488,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/62ab43_8c46e9fa8a3b43fab85f73137ed9138f.jpg"
+    },
+    {
+        id: 6,
+        url: "https://static.wixstatic.com/media/62ab43_847ae167711042aba078700896e61587.jpg/v1/fill/w_391,h_488,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/62ab43_847ae167711042aba078700896e61587.jpg"
+    },
+    {
+        id: 7,
+        url: "https://static.wixstatic.com/media/62ab43_7e1a8b82a0344693ba901406b2e2fcac.jpg/v1/fill/w_386,h_488,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/62ab43_7e1a8b82a0344693ba901406b2e2fcac.jpg"
+    },
+    {
+        id: 8,
+        url: "https://static.wixstatic.com/media/62ab43_847ae167711042aba078700896e61587.jpg/v1/fill/w_391,h_488,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/62ab43_847ae167711042aba078700896e61587.jpg"
+    },
+    {
+        id: 9,
+        url: "https://static.wixstatic.com/media/62ab43_7e1a8b82a0344693ba901406b2e2fcac.jpg/v1/fill/w_386,h_488,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/62ab43_7e1a8b82a0344693ba901406b2e2fcac.jpg"
+    }
+]
