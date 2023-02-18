6d919c0a0e85cc69015867933238ad6a4036f5d3
Thyago Lucas <thyago.devin@gmail.com>
2023-02-17 23:17:55 -0400

    feat: view publications

--- src/components/PublicationRender/index.tsx
+++ src/components/PublicationRender/index.tsx
@@ -4,6 +4,7 @@ import { PublicationContainer } from "./style";
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import 'react-lazy-load-image-component/src/effects/opacity.css';
 import { useState } from "react";
+import Modal from "../FirebaseImageGallery/Modal";
 
 interface PublicationProps {
     publications: {
@@ -14,40 +15,68 @@ interface PublicationProps {
 
 function PublicationRender(props: PublicationProps){
     const { publications } = props
-    const [images, setImages] = useState<{ id: number, url: string }[]>([])
+   
     const [currentSlide, setCurrentSlide] = useState(0)
     const [showModal, setShowModal] = useState(false)
 
     const handleNextSlide = () => {
-        images.length - 1 > currentSlide ? setCurrentSlide(currentSlide + 1) : setCurrentSlide(0)
+        publications.length - 1 > currentSlide ? setCurrentSlide(currentSlide + 1) : setCurrentSlide(0)
     }
 
     const handlePreviousSlide = () => {
-        currentSlide > 0 ? setCurrentSlide(currentSlide - 1) : setCurrentSlide(images.length - 1)
+        currentSlide > 0 ? setCurrentSlide(currentSlide - 1) : setCurrentSlide(publications.length - 1)
     }
 
     const handleClose = () => {
         setShowModal(false)
     }
 
+    document.onkeydown = (event: KeyboardEvent) => {
+        if (event.key === "ArrowRight") {
+            handleNextSlide()
+        } else if (event.key === "ArrowLeft") {
+            handlePreviousSlide()
+        } else if (event.key === "Escape") {
+            handleClose()
+        }
+    }
+
     return(
         <Transition>
             <PublicationContainer>
                 <h5>{translate("booksAndMagazines")}</h5>
-                <div>
+                <div className="rendering">
                     {
                         publications.map((value, index)=> {
                             return <LazyLoadImage 
                                         threshold={30}
                                         src={value.url} 
                                         key={index} 
-                                        effect="opacity"/>
+                                        effect="opacity"
+                                        onClick={() => {
+                                           
+                                            setShowModal(true)
+                                            setCurrentSlide(index)
+                                        }}
+                                        />
                         })
-                    }
+                    }                    
                 </div>
+
+                {
+                    showModal? <Modal 
+                        imagePath={publications[currentSlide].url}
+                        nextSlide={handleNextSlide}
+                        previousSlide={handlePreviousSlide}
+                        close={handleClose}
+                    
+                    /> :<></>
+                }
             </PublicationContainer>
         </Transition>
     )
 }
+
+
 
 export default PublicationRender;
